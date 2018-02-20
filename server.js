d1d787ccce9955829a1dab2cdea4e6523de48569
Roger Jiang <dkra4741@gmail.com>
2018-02-20 20:51:08 +0800

    docker compose with volumes

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const Koa = require('koa2')
 const app = new Koa()
 
 app.use(async function(ctx) {
-  ctx.body = 'Test automated rebuild when code push to Github!'
+  ctx.body =
+    'Docker volumes can make the data persist util the container stopped!!!!!'
 })
 
 app.listen(3000)
